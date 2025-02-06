97545b714f62ad2c1672eba57689200d834b5ed2
piezhok <pirog.max83@gmail.com>
2025-02-06 19:54:17 +0300

    Update SentView.vue

--- src/views/SentView.vue
+++ src/views/SentView.vue
@@ -1,18 +1,18 @@
 <script setup>
     import {onMounted, ref} from "vue";
 
-    async function getSmth() {
-        const smth = await fetch("http://45.82.253.8:5000/api/cards/sent/", {
-            method: 'GET',
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-        const response = await smth.json();
-        console.log(response);
-        return response;
-    }
+    // async function getSmth() {
+    //     const smth = await fetch("http://45.82.253.8:5000/api/cards/sent/", {
+    //         method: 'GET',
+    //         mode: 'no-cors',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         }
+    //     });
+    //     const response = await smth.json();
+    //     console.log(response);
+    //     return response;
+    // }
 
     const test = ref();
     onMounted(() => {
