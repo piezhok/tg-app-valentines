7a072af42453e62c1bda2a638d94d646ab50e427
piezhok <pirog.max83@gmail.com>
2025-02-06 19:02:05 +0300

    db test

--- src/views/SentView.vue
+++ src/views/SentView.vue
@@ -1,5 +1,23 @@
 <script setup>
+    import {onMounted} from "vue";
 
+    async function getSmth() {
+        const smth = await fetch("http://45.82.253.8:5000/api/cards/sent/", {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        const response = await smth.json();
+        console.log(response);
+        return response;
+    }
+
+    onMounted(() => {
+        const test = getSmth();
+        console.log(test);
+    })
 </script>
 
 <template>
