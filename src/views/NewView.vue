1803a3f43727bcdebb1369aff0ce3dc6dceddce3
piezhok <pirog.max83@gmail.com>
2025-02-11 10:51:42 +0300

    Update NewView.vue

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios';
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {useRouter} from "vue-router";
 const addAt = (event) => {
     const input = event.target;
