d93d041659ac5d4174add74bcf68c13b6d00e9c5
piezhok <pirog.max83@gmail.com>
2025-02-11 10:40:52 +0300

    testnew

--- src/App.vue
+++ src/App.vue
@@ -47,10 +47,10 @@ onMounted(async () => {
     const initData = window.Telegram.WebApp.initData;
     const params = new URLSearchParams(initData);
     const userId = JSON.parse(params.get("user")).id;
-    await window.Telegram.WebApp.CloudStorage.getItem("test_id", async (success, value) => {
+    await window.Telegram.WebApp.CloudStorage.getItem("user_id", async (success, value) => {
         if (value == "") {
             const post = await postUser(initData, userId);
-            await window.Telegram.WebApp.CloudStorage.setItem("test_id", post);
+            await window.Telegram.WebApp.CloudStorage.setItem("user_id", post);
         } else {
             console.log("success", value, success);
         }
--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -14,27 +14,29 @@ const addAt = (event) => {
 
 const router = useRouter();
 const store = useStore()
-const initData = computed(() => {
-    return store.state.initData;
-})
-const userInfo = computed(() => {
-    return store.getters.userInfo;
-})
+// const initData = computed(() => {
+//     return store.state.initData;
+// })
+// const userInfo = computed(() => {
+//     return store.getters.userInfo;
+// })
 // const initData = ref();
 // const userInfo = ref();
 // const senderId = ref();
 
-const formData = ref({
-    telegram_init_data: "user=%7B%22id%22%3A404535053%2C%22first_name%22%3A%22%D1%8D%D1%82%D0%BE%20%D0%BD%D0%B5%20%D0%BF%D0%B8%D1%80%D0%BE%D0%B3%E2%81%B4%C2%B2%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22piezhok%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FVvqVW87n5WkdJX8uzHL6O8EhVGnWgsrthtTX3YosfQM.svg%22%7D&chat_instance=8787168753902846877&chat_type=private&auth_date=1738859307&signature=fGWpwSYIQ0KLXhxO7nA8KoOt7ms1Y5-CPGnSVdVuQvfdRcRGDLL4KG7eWALkv9egB4Ni7heLoigevFsYIHrIAQ&hash=a1dd914b90a7b78eabbff79ff99a6dc214668c658926c5d37a604490ae6bf69a",
-    sender_telegram_id: 404535053,
-    receiver_telegram_id: 488687700,
-    message: "",
-    created_at: 0,
-    anonymous: true
-});
+const initData = window.Telegram.WebApp.initData;
+const params = new URLSearchParams(initData);
+const userId = JSON.parse(params.get("user")).id;
+
+// const formData = ref({
+//     telegram_init_data: initData,
+//     sender_telegram_id: userId,
+//     receiver_telegram_id: 488687700,
+//     message: "",
+//     created_at: 0,
+//     anonymous: true
+// });
 onMounted(() => {
-    formData.value.telegram_init_data = initData.value;
-    formData.value.sender_telegram_id = userInfo.value.id;
 })
 
 const removeAt = (event) => {
@@ -47,7 +49,14 @@ const removeAt = (event) => {
 
 async function submitForm() {
     try {
-        const response = await axios.post('https://saharvnor.me:5000/api/cards', formData.value);
+        const response = await axios.post('https://saharvnor.me:5000/api/cards', {
+            telegram_init_data: initData,
+            sender_telegram_id: userId,
+            receiver_telegram_id: 488687700,
+            message: "",
+            created_at: 0,
+            anonymous: true
+        });
         console.log('Form submitted successfully', response);
         router.to = "/sent";
     } catch (error) {
