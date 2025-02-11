a0bb776fae6ac6d2db85bd2c70e3b3f381e51a1d
piezhok <pirog.max83@gmail.com>
2025-02-11 10:17:08 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ const postUser = async (data, id) => {
 
 // const passPhrase = ref()
 onMounted(async () => {
-    await window.Telegram.WebApp.CloudStorage.removeItem("telegram_id");
+    await window.Telegram.WebApp.CloudStorage.removeItem("user_id");
     const initData = window.Telegram.WebApp.initData;
     const params = new URLSearchParams(initData);
     const userId = JSON.parse(params.get("user")).id;
