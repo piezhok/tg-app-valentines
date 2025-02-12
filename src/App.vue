3547b4bacdcad714f330d8d5d35e2f1aa6581ad3
piezhok <pirog.max83@gmail.com>
2025-02-12 15:10:54 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ onMounted(async () => {
             console.log("success", value, success);
         }
     })
-    await store.dispatch("getLetters", [userId, decodeURI(initData)]);
+    await store.dispatch("getLetters", [userId, initData]);
 })
 
 // onBeforeMount(() => {
