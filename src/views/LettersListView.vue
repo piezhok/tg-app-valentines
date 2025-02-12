b14a5df35dbd7f5a8dd1123ef4ce36f6050fc466
piezhok <pirog.max83@gmail.com>
2025-02-12 23:39:37 +0300

    Update LettersListView.vue

--- src/views/LettersListView.vue
+++ src/views/LettersListView.vue
@@ -21,7 +21,7 @@ const getUserValue = (letterslist, i, value) => {
 }
 
 const getAvatar = async (letterslist, i) => {
-    while (await letterslist[i]["anonymous"] == undefined) {getAvatar(letterslist, i)}
+    console.log("getAvatar", letterslist[i]["anonymous"]);
     if (await letterslist[i]["anonymous"] === true) {
         return "@/assets/anon.svg";
     } else {
