0341c62d82836889c49ec4789766d025d21b9e5a
piezhok <pirog.max83@gmail.com>
2025-02-12 12:10:28 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -46,10 +46,8 @@ export default new createStore({
         },
         async getLetters({ commit, userid, init }) {
             const response = await axios.get('https://saharvnor.me:5000/api/cards/', {
-                params: {
-                    "user_id": userid,
-                    "telegram_init_data": init
-                }
+                "user_id": userid,
+                "telegram_init_data": init
             });
             const data = await response.data;
             await commit('setReceived', data.received);
